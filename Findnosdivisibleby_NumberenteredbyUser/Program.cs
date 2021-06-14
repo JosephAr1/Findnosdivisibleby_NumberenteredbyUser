84892778dde84b3f514fe9906ef2a70750a237c5
JosephAr1 <jonielhet@yahoo.com>
2021-06-14 20:13:42 +1200

    Add project files.

---
+++ Findnosdivisibleby_NumberenteredbyUser/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Findnosdivisibleby_NumberenteredbyUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, i;
+            
+            Console.WriteLine("Please enter a number:");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            for(i=1; i<=10;i++)
+            {                
+             Console.WriteLine(number1.ToString() + " * " + i + " = " + (number1).ToString());           
+             }
+        }
+    }
+}
